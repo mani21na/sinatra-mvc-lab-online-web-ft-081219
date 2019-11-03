64b9775a0de9cf62ec2519178e029fa6a78f4d4f
Hanna Lee <mani21na@gmail.com>
2019-11-03 02:42:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+  get '/' do 
+    erb :user_input
+  end 
+  
+  post '/' do
+    @word = PigLatinizer.new(params[:user_phrase])
+    binding.pry
+    erb :result
+  end 
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,14 @@
+class PigLatinizer 
+  attr_reader :phrase
+  
+  def initialize(user_phrase)
+    @phrase = user_phrase
+  end 
+  
+  def latinizer 
+    words_array = phrase.split(" ")
+    word.map do |word|
+      word.
+    end 
+   
+end 
